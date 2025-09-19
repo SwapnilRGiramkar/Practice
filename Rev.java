85fb78e67aa0158bc907e9f2d700c1bc7b46a6dc
SwapnilRGiramkar <swapnilrajmira9764@gmail.com>
2025-09-19 12:46:52 +0530

    Reverse the Given String

---
+++ Rev.java
@@ -0,0 +1,22 @@
+//Reverse the given String 
+package strings;
+//Reverse a string without using built-in functions.
+public class Rev 
+{
+	public static void main(String[] args) 
+	{
+		String s="Swapnil Giramkar";
+		String ans=reverse(s);
+		System.out.println(ans);
+	}
+	public static String reverse(String s)
+	{
+		String ans="";
+		for(int i=s.length()-1;i>=0;i--)
+		{
+			ans=ans+s.charAt(i);
+		}
+		return ans;
+	}
+}
+
